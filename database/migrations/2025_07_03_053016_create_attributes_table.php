6bc3f457c5d98de9b58fc2e2a77680bf33d5a428
xhadix <hadixakbar@gmail.com>
2025-07-03 13:30:39 +0700

    fix attributes table issue

--- database/migrations/2025_07_03_053016_create_attributes_table.php
+++ database/migrations/2025_07_03_053016_create_attributes_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('attributes', function (Blueprint $table) {
+            $table->id();
             $table->string('name')->unique();
             $table->string('display_name');
             $table->boolean('is_active')->default(true);
